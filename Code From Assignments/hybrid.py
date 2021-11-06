c44ddb6dcdd49ec5fffcd5db96f31d3528fbc463
Palmer Edholm <palmer.jackiechan@gmail.com>
2021-11-06 13:56:38 -0600

    added fixed source code for hybrid method

--- Code From Assignments/hybrid.py
+++ Code From Assignments/hybrid.py
@@ -4,16 +4,17 @@ def hybrid(a, b, x0, f, df, tol, max_iter):
     # Initialize needed variables
     fa = f(a)
     fb = f(b)
-    if fa * fb >0:
+    if fa * fb > 0:
         raise Exception('Root not in interval')
     if x0 < a or x0 > b:
         raise Exception('Invalid initial guess')
     f0 = f(x0)
     df0 = df(x0)
-    error = 10*tol
-    icnt=0
+    error = 10.0*tol
+    errnewt = 10.0*tol
+    icnt = 0
     # Start Newton's method
-    while error > tol and icnt < max_iter:
+    while errnewt > tol and icnt < max_iter:
         x1 = x0-f0/df0
         errnewt = abs(x1-x0)
         # Perform 4 iterations of bisection if needed
@@ -21,16 +22,19 @@ def hybrid(a, b, x0, f, df, tol, max_iter):
             for i in range(4):
                 c = 0.5*(a+b)
                 fc = f(c)
-                if fa*fc<0:
-                    b=c
+                if fa*fc < 0:
+                    b = c
+                    fb = fc
                 else:
-                    a=c
-        error = abs(x1-x0)
-        x0 = x1
-        f0 = f(x0)
-        df0 = df(x0)
+                    a = c
+                    fa = fc
+            error = abs(b-a)
+            x0 = c
+            f0 = f(c)
+            df0 = df(c)
+        else:
+            x0 = x1
+            f0 = f(x0)
+            df0 = df(x0)
         icnt += 1
     return x1
-
-print(hybrid(0.5, 1.0, 0.9, lambda x: x*np.e**(3*x**2)-7*x, lambda x: np.e**(3*x**2)+6*(x**2)*np.e**(3*x**2)-7,
-             0.0001, 100))
