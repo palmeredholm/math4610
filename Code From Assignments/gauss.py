7d0c19864c152d1f813ed28028b4af0cb11591e3
Palmer Edholm <palmer.jackiechan@gmail.com>
2021-11-20 14:31:13 -0700

    fixed error in gauss code

--- Code From Assignments/gauss.py
+++ Code From Assignments/gauss.py
@@ -4,6 +4,7 @@ import numpy as np
 def gauss(A, b):
     # Copy the matrix that was passed in to reduce and initialize n for loops
     A = np.copy(A)
+    b = np.copy(b)
     n = len(b)
     # Traverse columns 1 to n-1
     for k in range(n - 1):
